a42228200d4ccac7da45a7b60fb3bf1ca4ea8129
yashgajare <116647797+yashgajare@users.noreply.github.com>
2023-07-05 11:03:25 +0530

    Added solution - LeetHub

---
+++ Stock buy and sell II - GFG/stock-buy-and-sell-ii.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int stockBuyAndSell(int n, vector<int> &prices) {
+        // code here
+        bool res = is_sorted(prices.begin(),prices.end());
+        int mn=INT_MAX, mx=0;
+        
+        if(!res){
+            for(int i=0;i<prices.size()-1;i++){
+                if(prices[i]<prices[i+1]){
+                    mx=mx+(prices[i+1]-prices[i]);
+                }
+            }
+            return mx;
+        }
+        else{
+            mn=prices[prices.size()-1]-prices[0];
+            return mn;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> prices(n);
+        Array::input(prices,n);
+        
+        Solution obj;
+        int res = obj.stockBuyAndSell(n, prices);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
