16cc99d5a4ae405bebbe53087d1644853d4b2e78
Yash Gajare <116647797+yashgajare@users.noreply.github.com>
2023-07-22 09:19:48 +0530

    Added solution - LeetHub

--- Remove duplicates from an unsorted linked list - GFG/remove-duplicates-from-an-unsorted-linked-list.cpp
+++ Remove duplicates from an unsorted linked list - GFG/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -44,24 +44,20 @@ class Solution
     Node * removeDuplicates( Node *head) 
     {
      // your code goes here
-     map<int,bool>mp;
-     Node* tmp=head;
-     Node* t=tmp;
-     while(tmp!=NULL){
-        if(mp[tmp->data]==false)
-        {
-            mp[tmp->data]=true;
-            t=tmp; 
-            tmp=tmp->next;
+        map<int,bool>mp;
+        Node* tmp=head;
+        Node* prev=tmp;
+        while(tmp!=NULL){
+            if(mp[tmp->data]==false){
+                mp[tmp->data] = true;
+                prev=tmp;
+                tmp=tmp->next;
+            }else{
+                prev->next=tmp->next;
+                tmp=tmp->next;
+            }
         }
-        else{
-            Node* del=t->next;
-            delete(del);
-            t->next=tmp->next;
-            tmp=tmp->next;
-        }
-     }
-     return head;
+        return head;
     }
 };
 
